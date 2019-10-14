dc88c17e780aaa9f448cc9bd4077f715ef8aec85
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-10-14 12:44:52 +0530

    added part 20

--- src/Part19.kt
+++ src/Part19.kt
@@ -2,9 +2,9 @@ fun main() {
     Greet(name = "Hans")
     Greet(reps = 5)
     Greet(message = "Hi")
-    getSum(1, 2)
-    getSum(num3 = 4, num1 = 2)
-    getSum(1, 2, 3, 4)
+    println(getSum(1, 2))
+    println(getSum(num3 = 4, num1 = 2))
+    println(getSum(1, 2, 3, 4))
 }
 
 fun Greet(message: String = "Nothing", name: String = "User", reps: Int = 1) {
@@ -12,6 +12,6 @@ fun Greet(message: String = "Nothing", name: String = "User", reps: Int = 1) {
         println("Hello, $name! $message")
 }
 
-fun getSum(num1: Int = 0, num2: Int = 0, num3: Int = 0, num4: Int = 0) {
-    println(num1 + num2 + num3 + num4)
+fun getSum(num1: Int = 0, num2: Int = 0, num3: Int = 0, num4: Int = 0): Int {
+    return num1 + num2 + num3 + num4
 }
---
+++ src/Part20.kt
@@ -0,0 +1,25 @@
+fun main() {
+    val array = intArrayOf(1, 2, 3, 4, 50, 6, 7, 8, 9)
+    println(getSum(10, 20, *array))
+    println(getLarge(1, *array))
+}
+
+fun getSum(vararg numbers: Int): Int {
+    var sum = 0
+    for (number in numbers)
+        sum += number
+    return sum
+}
+
+fun getLarge(a: Int, vararg numbers: Int): Int {
+    var array = intArrayOf(*numbers)
+    var l = a;
+    var L = array.size
+    --L
+    for (i:Int in 0..L)
+    {
+        if (array[i] > l)
+            l = array[i]
+    }
+    return l
+}
